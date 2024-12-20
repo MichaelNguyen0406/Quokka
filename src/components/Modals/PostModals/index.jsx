b7b37c8ea05b2677d0147e41bf636e1b1f00d12d
Michael <nguyentruongano914@gmail.com>
2024-12-20 17:24:26 +0700

    add file

--- src/components/Modals/PostModals/index.jsx
+++ src/components/Modals/PostModals/index.jsx
@@ -1,5 +1,4 @@
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import Input from "@mui/material/Input";
 import CloseIcon from "@mui/icons-material/Close";
@@ -7,6 +6,14 @@ import IconButton from "@mui/material/IconButton";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import InsertPhotoIcon from "@mui/icons-material/InsertPhoto";
 import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+
+// Import Emoji Picker
+import EmojiPicker from "emoji-picker-react";
+
+// Import React
+import { useState } from "react";
 
 const style = {
   borderRadius: "20px",
@@ -14,7 +21,7 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  width: 700,
   bgcolor: "background.paper",
   boxShadow: 24,
   p: "1rem",
@@ -22,6 +29,27 @@ const style = {
 
 // eslint-disable-next-line react/prop-types
 function PostModals({ open, onClose }) {
+  // Menu
+  const [anchorEl, setAnchorEl] = useState(null);
+  const openEmoji = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  // Input
+  const [valueInput, setValueInput] = useState("");
+  const getEmoji = ({ emoji }) => {
+    setValueInput(valueInput + emoji);
+  };
+  const handleChangeInput = (e) => {
+    setValueInput(e.target.value);
+  };
+
+  // Image
+  const [images, setImages] = useState([]);
+
   return (
     <Modal
       open={open}
@@ -31,7 +59,7 @@ function PostModals({ open, onClose }) {
     >
       <Box sx={style}>
         <Box sx={{ m: "-0.5rem 0 0.5rem -0.5rem" }}>
-          <IconButton>
+          <IconButton onClick={onClose}>
             <CloseIcon />
           </IconButton>
         </Box>
@@ -45,20 +73,38 @@ function PostModals({ open, onClose }) {
           <Box>
             <AccountCircleIcon sx={{ fontSize: 45 }} />
           </Box>
-          <Input
-            autoFocus={true}
-            fullWidth
-            disableUnderline
-            placeholder="What is happening?"
-          />
+          <Box>
+            <Input
+              sx={{ fontSize: "1.5rem" }}
+              autoFocus={true}
+              fullWidth
+              disableUnderline
+              placeholder="What is happening?"
+              value={valueInput}
+              onChange={handleChangeInput}
+            />
+          </Box>
         </Box>
-        <Box pt="1rem">
+        <Box pt="1rem" display="flex" alignItems="center">
           <IconButton>
             <InsertPhotoIcon />
           </IconButton>
-          <IconButton>
-            <EmojiEmotionsIcon />
-          </IconButton>
+          <Box>
+            <IconButton onClick={handleClick}>
+              <EmojiEmotionsIcon />
+            </IconButton>
+            <Menu
+              transformOrigin={{ vertical: "bottom", horizontal: "center" }}
+              anchorOrigin={{ vertical: "top", horizontal: "center" }}
+              anchorEl={anchorEl}
+              open={openEmoji}
+              onClose={handleClose}
+            >
+              <MenuItem>
+                <EmojiPicker onEmojiClick={getEmoji} />
+              </MenuItem>
+            </Menu>
+          </Box>
         </Box>
       </Box>
     </Modal>
